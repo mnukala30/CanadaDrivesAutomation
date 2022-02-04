2595495cab83ad0b9e11aafbf9cf1b92162846a3
mnukala30 <mnukala30@gmail.com>
2022-02-04 22:46:28 +0530

    Initial Commit

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"b41d3426-2715-4068-8940-cf6eb0437bbd","feature":"Purchase a car through Canada Drives Website","scenario":"Pick available RAM vehicle in desired province","start":1643993599481,"group":17,"content":"","tags":"@order-creation,","end":1643993649643,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"1cd8bd1b-715a-4e67-9a05-c1cbb2eabac8","feature":"Purchase a car through Canada Drives Website","scenario":"Pick available RAM vehicle in desired province","start":1643994917961,"group":17,"content":"","tags":"@order-creation,","end":1643994932863,"className":"failed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":17,"content":"Thread[TestNG-PoolService-0,5,main]"}]);
 });
